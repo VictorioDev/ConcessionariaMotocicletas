3835741092b57c7e9b7bd299898a1120fd309a1b
lucianoajunior <luciano@lucianojunior.com.br>
2018-04-12 01:52:02 -0300

    Ajustes templates, clientes e painel

--- ConcessionariaDeMotocicletas/src/java/br/ads/concessionaria/controller/UsuarioController.java
+++ ConcessionariaDeMotocicletas/src/java/br/ads/concessionaria/controller/UsuarioController.java
@@ -1,26 +1,144 @@
 package br.ads.concessionaria.controller;
 
+import br.ads.concessionaria.dao.UsuarioDAO;
+import br.ads.concessionaria.domain.Usuario;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
- *
- * @author Victorio Zansavio
+ * Controller de Usuários
+ * @author Luciano Carvalho
  */
-
 @Controller
 public class UsuarioController {
     
-    @RequestMapping("usuarios/novo}")
-    public String cadastroUsuario(){
-        return "usuario";
+    /**
+     * Método pricipal que lista todos os usuários do sistema.
+     * @param m
+     * @return 
+     */
+    @RequestMapping("usuarios")
+    public ModelAndView usuarios( Model m ) {
+        
+        ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+                
+        try {
+            listaUsuarios = UsuarioDAO.listarUsuario();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        m.addAttribute("usuarios", listaUsuarios );
+        
+        return new ModelAndView("usuarios/IndexView");
     }
     
-   
+    /**
+     * Método que carrega os dados do usuário para visualização.
+     * @param id
+     * @return 
+     */
+    @RequestMapping( value = "usuarios/visualizar/{id}", method = RequestMethod.GET )
+    public ModelAndView visualizar( @PathVariable("id") int id ) {
+        
+        Usuario u = new Usuario();
+        
+        try {
+            u = UsuarioDAO.retornarUsuarioPorId(id);
+        } catch( SQLException ex ) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return new ModelAndView("usuarios/VisualizarView", "usuario", u );
+    }
+    
+    /**
+     * Método para exibir o formulário de cadastro.
+     * @return 
+     */
+    @RequestMapping(value = "/usuarios/cadastrar", method = RequestMethod.GET )
+    public ModelAndView cadastrar() {
+        return new ModelAndView("usuarios/CadastrarView", "usuario", new Usuario() );
+    }
+    
+    /**
+     * Método que recebe o envio do formulário de cadastro.
+     * @param u
+     * @return 
+     */
+    @RequestMapping(value = "/usuarios/cadastrar", method = RequestMethod.POST )
+    public String cadastrar( @ModelAttribute("usuario") Usuario u ) {
+        try {
+            UsuarioDAO.incluirUsuario( u );
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return "redirect:/usuarios";
+    }
+    
+    /**
+     * Método para carregar o formulário de edição de um usuário.
+     * @param id
+     * @return 
+     */
+    @RequestMapping( value = "usuarios/editar/{id}", method = RequestMethod.GET )
+    public ModelAndView editar( @PathVariable("id") int id ) {        
+        
+        Usuario u = new Usuario();
+        
+        try {
+            u = UsuarioDAO.retornarUsuarioPorId( id );    
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return new ModelAndView("usuarios/EditarView", "usuario", u );
+    }
+    
+    /**
+     * Método que recebe a requisição de edição de um usuário.
+     * @param u
+     * @return 
+     */    
+    @RequestMapping( value = "usuarios/editar/{id}", method = RequestMethod.POST )
+    public String editar( @ModelAttribute("usuario") Usuario u ) {
+       
+        try {
+            UsuarioDAO.alterarUsuario( u );
+        } catch( SQLException ex ) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return "redirect:/usuarios";
+    }
+    
+    /**
+     * Método para remover um usuário do sistema.
+     * @param id
+     * @return 
+     */
+    @RequestMapping( value = "usuarios/remover/{id}", method = RequestMethod.GET )
+    public String remover( @PathVariable("id") int id ) {
+        
+        Usuario u = new Usuario();
+        u.setIdUsuario( id );
+        
+        try {
+            UsuarioDAO.removerUsuario( u );
+        } catch( SQLException ex ) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return "redirect:/usuarios";
+    }
 }
---
+++ ConcessionariaDeMotocicletas/web/WEB-INF/jsp/usuarios/IndexView.jsp
@@ -0,0 +1,62 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+
+<t:import url="../templates/header.jsp"/>
+
+<h3>Usuários</h3>
+<a href="<t:url value="/usuarios/cadastrar" />" class="btn btn-primary"><span class="oi" data-glyph="plus" title="Acessórios" aria-hidden="true"></span> Novo usuário</a>
+
+<form method="GET">
+    <div class="container-fluid mt-2">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="row">
+                    <input type="text" id="search" name="search" class="form-control col-md-10" placeholder="Digite a sua busca...">
+                    <button type="submit" class="btn btn-sm btn-secondary col-md-2">Buscar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<table class="table table-hover mt-2">
+    <thead>
+        <th>#</th>
+        <th>Usuário</th>
+        <th>Tipo</th>
+        <th>Login</th>
+        <th>Endereço</th>
+        <th>Telefone</th>
+        <th>Ações</th>
+    </thead>
+    <tbody>
+        <t:forEach items="${usuarios}" var="usuario">
+            <tr>
+                <td><t:out value="${usuario.idUsuario}" /></td>
+                <td><t:out value="${usuario.nome}" /></td>
+                <td><t:out value="${usuario.tipo}" /></td>
+                <td><t:out value="${usuario.login}" /></td>
+                <td><t:out value="${usuario.endereco}" /></td>
+                <td><t:out value="${usuario.telefone}" /></td>
+                <td>
+                    <a href="<t:url value="/usuarios/visualizar/${usuario.idUsuario}" />" class="btn btn-sm btn-primary">
+                        <span class="oi" data-glyph="eye" title="Visualizar" aria-hidden="true"></span> Visualizar
+                    </a>
+                    <a href="<t:url value="/usuarios/editar/${usuario.idUsuario}"/>" class="btn btn-sm btn-secondary">
+                        <span class="oi" data-glyph="pencil" title="Editar" aria-hidden="true"></span> Editar
+                    </a>
+                    <a href="<t:url value="/usuarios/remover/${usuario.idUsuario}"/>" class="btn btn-sm btn-red">
+                        <span class="oi" data-glyph="x" title="Remover" aria-hidden="true"></span> Remover
+                    </a>
+                </td>
+            </tr>
+        </t:forEach>
+    </tbody>
+</table>
+
+<p><b>Total:</b> ${usuarios.size()} registros.</p>
+
+<t:import url="../templates/footer.jsp"/>
